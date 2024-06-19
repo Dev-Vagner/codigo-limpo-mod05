074ca7b5f3f3c635183af13e5d882c40380a1923
Vagner Bruno <vagnerbackend@gmail.com>
2024-06-19 09:55:44 -0300

    Finalizei o exercício 1

--- src/test/java/br/com/vbruno/exercicio1/CalculadoraTest.java
+++ src/test/java/br/com/vbruno/exercicio1/CalculadoraTest.java
@@ -13,4 +13,41 @@ public class CalculadoraTest {
 
         Assert.assertEquals(resultadoEsperado, resultadoRetornado);
     }
+
+    @Test
+    public void testSubtrair_HappyPath() {
+        Calculadora calculadora = new Calculadora();
+        int resultadoEsperado = 1;
+
+        int resultadoRetornado = calculadora.subtrair(3, 2);
+
+        Assert.assertEquals(resultadoEsperado, resultadoRetornado);
+    }
+
+    @Test
+    public void testMultiplicar_HappyPath() {
+        Calculadora calculadora = new Calculadora();
+        int resultadoEsperado = 6;
+
+        int resultadoRetornado = calculadora.multiplicar(3, 2);
+
+        Assert.assertEquals(resultadoEsperado, resultadoRetornado);
+    }
+
+    @Test
+    public void testDividir_HappyPath() {
+        Calculadora calculadora = new Calculadora();
+        int resultadoEsperado = 2;
+
+        int resultadoRetornado = calculadora.dividir(6, 3);
+
+        Assert.assertEquals(resultadoEsperado, resultadoRetornado);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDividir_ErroDivisaoPorZero() {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.dividir(3, 0);
+    }
 }
